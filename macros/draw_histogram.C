31a2ebf71e4c685eb8dedca9b51fa94051599f57
Peter Young <peter.young@cern.ch>
2022-08-16 10:09:27 -0500

    fixed issue with l-jets

--- macros/draw_histogram.C
+++ macros/draw_histogram.C
@@ -7,6 +7,7 @@
 void draw_histogram(std::string histogram_name) 
 {
   std::string filepath = "../QCD_results/results_200to300.root";
+  filepath = "../test.root";
   //std::string histogram_name = "evt_cutflow";
 
   TFile *f = new TFile(filepath.c_str());
