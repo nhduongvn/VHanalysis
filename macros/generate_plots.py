12863ed598379f0adb1ee66adc412dec646643f1
Peter Young <peteryou@buffalo.edu>
2022-09-01 12:55:31 -0400

    updated plots

--- macros/generate_plots.py
+++ macros/generate_plots.py
@@ -19,21 +19,27 @@ plots = [ "HMass", "dR_H", "dPhi_H", "H_pt_jet0", "H_eta_jet0", "H_phi_jet0",
 "Z_eta_jet2", "Z_phi_jet2", "pt_jet", "eta_jet", "phi_jet", "mSV_jet", "Njet"]
 
 x_axis = {
- "HMass": "m_{c#bar{c}}", "dR_H": "#Delta R_{c#bar{c}}",
- "dPhi_H": "#Delta#phi_{c#bar{c}}", "H_pt_jet0": "p_{T,j0}^{H}",
+ "HMass": "m_{c#bar{c}} [GeV]", "dR_H": "#Delta R_{c#bar{c}}",
+ "dPhi_H": "#Delta#phi_{c#bar{c}}", "H_pt_jet0": "p_{T,j0}^{H} [GeV]",
  "H_eta_jet0": "#eta_{j0}^{H}", "H_phi_jet0": "#phi_{j0}^{H}",
- "H_pt_jet1": "p_{T,j1}^{H}", "H_eta_jet1": "#eta_{j1}^{H}", 
- "H_phi_jet1": "#phi_{j1}^{H}", "H_pt_jet2": "p_{T,j2}^{H}",
+ "H_pt_jet1": "p_{T,j1}^{H} [GeV]", "H_eta_jet1": "#eta_{j1}^{H}", 
+ "H_phi_jet1": "#phi_{j1}^{H}", "H_pt_jet2": "p_{T,j2}^{H} [GeV]",
  "H_eta_jet2": "#eta_{j2}^{H}", "H_phi_jet2": "#phi_{j2}^{H}",
- "ZMass": "m_{b#bar{b}}",
+ "ZMass": "m_{b#bar{b}} [GeV]",
  "dR_Z": "#Delta R_{b#bar{b}}", "dPhi_Z": "#Delta#phi_{b#bar{b}}",
- "Z_pt_jet0": "p_{T,j0}^{Z}", "Z_eta_jet0": "#eta_{j0}^{Z}", 
- "Z_pZi_jet0": "#pZi_{j0}^{Z}", "Z_pt_jet1": "p_{T,j1}^{Z}", 
- "Z_eta_jet1": "#eta_{j1}^{Z}", "Z_pZi_jet1": "#pZi_{j1}^{Z}", 
- "Z_pt_jet2": "p_{T,j2}^{Z}", "Z_eta_jet2": "#eta_{j2}^{Z}", 
- "Z_pZi_jet2": "#pZi_{j2}^{Z}", "pt_jet": "p_{T}^{jet}",
+ "Z_pt_jet0": "p_{T,j0}^{Z} [GeV]", "Z_eta_jet0": "#eta_{j0}^{Z}", 
+ "Z_phi_jet0": "#phi_{j0}^{Z}", "Z_pt_jet1": "p_{T,j1}^{Z} [GeV]", 
+ "Z_eta_jet1": "#eta_{j1}^{Z}", "Z_phi_jet1": "#phi_{j1}^{Z}", 
+ "Z_pt_jet2": "p_{T,j2}^{Z} [GeV]", "Z_eta_jet2": "#eta_{j2}^{Z}", 
+ "Z_phi_jet2": "#phi_{j2}^{Z}", "pt_jet": "p_{T}^{jet} [GeV]",
  "eta_jet": "#eta^{jet}", "phi_jet": "#phi^{jet}",
- "mSV_jet": "m_{SV}^{jet}", "Njet": "N_{jet}",
+ "mSV_jet": "m_{SV}^{jet} [GeV]", "Njet": "N_{jet}",
+}
+
+y_axis = {
+ "pt": "Events / 1.0 GeV", "eta": "Events / 0.02",
+ "phi": "Events / 0.02", "mass": "Events / 1.0 GeV",
+ "dR": "Events / 0.1",
 }
 
 gen_plots = ["Higgs_mass", "Higgs_pt", "Higgs_phi", "Z_mass", "Z_pt", "Z_phi",
@@ -41,9 +47,9 @@ gen_plots = ["Higgs_mass", "Higgs_pt", "Higgs_phi", "Z_mass", "Z_pt", "Z_phi",
 "cc_dR", "bb_dR", "cc_dPhi", "bb_dPhi"]
 
 gen_x_axis = {
- "Higgs_mass": "m_{H}^{gen}", "Higgs_pt": "p_{T,H}^{gen}",
- "Higgs_phi": "#phi_{H}^{gen}",
- "Z_mass": "m_{Z}^{gen}", "Z_pt": "p_{T,Z}^{gen}", "Z_phi": "#phi_{Z}^{gen}",
+ "Higgs_mass": "m_{H}^{gen} [GeV]", "Higgs_pt": "p_{T,H}^{gen} [GeV]",
+ "Higgs_phi": "#phi_{H}^{gen}", "Z_mass": "m_{Z}^{gen} [GeV]", 
+ "Z_pt": "p_{T,Z}^{gen} [GeV]", "Z_phi": "#phi_{Z}^{gen}",
  "cjet_phi": "#phi_{c-jet}^{gen}", "bjet_phi": "#phi_{b-jet}^{gen}",
  "ljet_phi": "#phi_{l-jet}^{gen}", "HZ_dPhi": "#Delta#phi_{HZ}",
  "HZ_dR": "#Delta R_{HZ}", "Hc_dR": "#Delta R_{Hc}", 
@@ -51,6 +57,11 @@ gen_x_axis = {
  "bb_dR": "#Delta R_{bb}", "cc_dPhi": "#Delta#phi_{cc}^{gen}",
  "bb_dPhi": "#Delta#phi_{bb}^{gen}",
 }
+
+legX0 = 0.75
+legY0 = 0.65
+legX1 = 0.89
+legY1 = 0.89
 
 ## == Useful Methods ==========================================================
 
@@ -66,6 +77,17 @@ def scaleToLumi(fName, xSec, lumi):
   nP = hTmp.GetBinContent(3)
   nN = hTmp.GetBinContent(1)
   return float(lumi)*float(xSec)/float(nP-nN)
+
+def getYTitle(plt): 
+  yTitle = "Events"
+  if plt.find("pt") != -1: yTitle = y_axis["pt"]
+  elif plt.find("eta") != -1: yTitle = y_axis["eta"]
+  elif plt.find("phi") != -1 or plt.find("Phi") != -1: 
+    yTitle = y_axis["phi"]
+  elif plt.find("dR") != -1: yTitle = y_axis["dR"]
+  elif plt.find("Mass") != -1 or plt.find("mSV") != -1:
+    yTitle = y_axis["mass"]
+  return yTitle
 
 ## == Files of Interest =======================================================
 
@@ -88,6 +110,7 @@ root_files = {
 }
 
 shouldScaleToLumi = True
+useLogScale = False
 
 ## == Main Code ===============================================================
 
@@ -105,8 +128,9 @@ for yr in [18]:
 
     ## Create a stack on which we will stack the plots.
     hstack = ROOT.THStack("hs", "")
-    legend = ROOT.TLegend(0.75, 0.7, 0.89, 0.89)
-    legend.SetHeader("H#rightarrow cc Productions", "C")
+    legend = ROOT.TLegend(legX0, legY0, legX1, legY1)
+    legend.SetTextSize(0.04)
+    legend.SetHeader("Samples", "C")
     legend.SetBorderSize(0)
 
     ## Go through each sample & pull the proper plot.
@@ -128,10 +152,7 @@ for yr in [18]:
       if shouldScaleToLumi:
         scale = scaleToLumi(file_name, xSec[sample][yr], lumi[yr])
         hnew.Scale(scale)
-      hnew.GetYaxis().SetTitle("Events")
-      if plt in x_axis:
-        hnew.GetXaxis().SetTitle(x_axis[plt])
-     
+      
       legend.AddEntry(hnew, sample, "f")
       ## Draw the histogram to the canvas
       hstack.Add(hnew)
@@ -140,10 +161,17 @@ for yr in [18]:
     c = ROOT.TCanvas()
     c.cd()
     hstack.Draw("HIST")
-    hstack.GetYaxis().SetTitle("Events")
+    
+    yTitle = getYTitle(plt)
+    hstack.GetYaxis().SetTitle(yTitle)
     if plt in x_axis:
       hstack.GetXaxis().SetTitle(x_axis[plt])
     legend.Draw("same")
+    
+    scale = "linear"
+    if useLogScale:
+      scale = "log"
+      ROOT.gPad.SetLogy()
     
     ## Add some necessary labels
     t = ROOT.TLatex(); t.SetNDC()
@@ -163,7 +191,7 @@ for yr in [18]:
     
     ## Add some necessary labels to our canvas
     ## == LABELS GO HERE ==
-    savepath = "../saved_plots/" + plt + "_20" + str(yr) + ".png"
+    savepath = "../saved_plots/" + scale + "/" + plt + "_20" + str(yr) + ".png"
     c.SaveAs(savepath)
   
   ## == Go through each plot/histogram (Generator Object Plots) ==
@@ -171,8 +199,9 @@ for yr in [18]:
     
     ## Create a stack on which we will stack the plots.
     hstack = ROOT.THStack("hs", "")
-    legend = ROOT.TLegend(0.75, 0.7, 0.89, 0.89)
-    legend.SetHeader("H#rightarrow cc Productions", "C")
+    legend = ROOT.TLegend(legX0, legY0, legX1, legY1)
+    legend.SetTextSize(0.04)
+    legend.SetHeader("Samples", "C")
     legend.SetBorderSize(0)
 
     ## Go through each sample & pull the proper plot.
@@ -206,10 +235,16 @@ for yr in [18]:
     c = ROOT.TCanvas()
     c.cd()
     hstack.Draw("HIST")
-    hstack.GetYaxis().SetTitle("Events")
+    yTitle = getYTitle(plt)
+    hstack.GetYaxis().SetTitle(yTitle)
     if plt in gen_x_axis:
       hstack.GetXaxis().SetTitle(gen_x_axis[plt])
     legend.Draw("same")
+    
+    scale = "linear"
+    if useLogScale:
+      scale = "log"
+      ROOT.gPad.SetLogy()
     
     ## Add some necessary labels
     t = ROOT.TLatex(); t.SetNDC()
@@ -229,5 +264,5 @@ for yr in [18]:
     
     ## Add some necessary labels to our canvas
     ## == LABELS GO HERE ==
-    savepath = "../saved_plots/generator_" + plt + "_20" + str(yr) + ".png"
+    savepath = "../saved_plots/" + scale + "/generator_" + plt + "_20" + str(yr) + ".png"
     c.SaveAs(savepath)
