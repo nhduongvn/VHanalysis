be9e7417923e02d0881c07d30cb720c00d5c3253
Peter Young <peter.young@cern.ch>
2023-04-12 17:49:34 -0500

    Updated combined SingleMuon results

--- SubmitToCondor/createFileLists_v2.py
+++ SubmitToCondor/createFileLists_v2.py
@@ -6,7 +6,7 @@ import subprocess
 #########################################################
 samples_json = "../Dataset_lists/datasets_NANOAODv9_MC.txt"
 samples_json = "../Dataset_lists/datasets_QCD100to200.txt"
-samples_json = "../Dataset_lists/datasets_SingleMuon.json"
+samples_json = "../Dataset_lists/datasets_SingleMuon_combined.txt"
 outDir = '../FileLists/'
 os.system('mkdir ' + outDir)
 
